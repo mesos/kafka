0f1a8b1f82ca8a46b9ed3ebda627c52014a9693f
yohan <dmitry.pekar@gmail.com>
2015-03-16 19:46:20 +0200

    added CliTest.status|update|remove

--- src/test/ly/stealth/mesos/kafka/CliTest.scala
+++ src/test/ly/stealth/mesos/kafka/CliTest.scala
@@ -5,6 +5,7 @@ import org.junit.Assert._
 import java.util
 import scala.collection.JavaConversions._
 import java.io.{ByteArrayOutputStream, PrintStream}
+import Util.Period
 
 class CliTest extends MesosTestCase {
   var out: ByteArrayOutputStream = null
@@ -33,6 +34,19 @@ class CliTest extends MesosTestCase {
   }
 
   @Test
+  def status {
+    Scheduler.cluster.addBroker(new Broker("0"))
+    Scheduler.cluster.addBroker(new Broker("1"))
+    Scheduler.cluster.addBroker(new Broker("2"))
+
+    exec("status")
+    assertTrue("" + out, out.toString.contains("status received"))
+    assertTrue("" + out, out.toString.contains("id: 0"))
+    assertTrue("" + out, out.toString.contains("id: 1"))
+    assertTrue("" + out, out.toString.contains("id: 2"))
+  }
+
+  @Test
   def add {
     exec("add 0 --cpus=0.1 --mem=128")
     assertTrue("" + out, out.toString.contains("Broker added"))
@@ -43,6 +57,29 @@ class CliTest extends MesosTestCase {
     val broker = Scheduler.cluster.getBroker("0")
     assertEquals(0.1, broker.cpus, 0.001)
     assertEquals(128, broker.mem)
+  }
+
+  @Test
+  def update {
+    val broker = Scheduler.cluster.addBroker(new Broker("0"))
+
+    exec("update 0 --failoverDelay=10s --failoverMaxDelay=20s --options=log.dirs=/tmp/kafka-logs")
+    assertTrue("" + out, out.toString.contains("Broker updated"))
+    assertTrue("" + out, out.toString.contains("delay:10s, maxDelay:20s"))
+    assertTrue("" + out, out.toString.contains("options: log.dirs=/tmp/kafka-logs"))
+
+    assertEquals(new Period("10s"), broker.failover.delay)
+    assertEquals(new Period("20s"), broker.failover.maxDelay)
+    assertEquals("log.dirs=/tmp/kafka-logs", broker.options)
+  }
+
+  @Test
+  def remove {
+    Scheduler.cluster.addBroker(new Broker("0"))
+    exec("remove 0")
+
+    assertTrue("" + out, out.toString.contains("Broker 0 removed"))
+    assertNull(Scheduler.cluster.getBroker("0"))
   }
 
   def exec(cmd: String) {
