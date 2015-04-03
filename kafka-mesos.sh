6c119413c0618574d2514ec2117f3f1995fed7c2
yohan <dmitry.pekar@gmail.com>
2015-04-03 16:43:24 +0300

    added check for jars count (version raise case)

--- kafka-mesos.sh
+++ kafka-mesos.sh
@@ -1,2 +1,19 @@
 #!/bin/sh
-java -jar kafka-mesos*.jar "$@"
+jar='kafka-mesos*.jar'
+
+check_jar() {
+    jars=$(find . -maxdepth 1 -name "$jar" | wc -l)
+
+    if [ $jars -eq 0 ]
+    then
+        echo "$jar not found"
+        exit 1
+    elif [ $jars -gt 1 ]
+    then
+        echo "More than one $jar found"
+        exit 1
+    fi
+}
+
+check_jar
+java -jar $jar "$@"
